a36cdad36a34ac84e5884558be34f262a54a5487
jlzhang <2506988720@qq.com>
2020-04-23 15:43:39 +0800

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello jenkins !";
+    private final String message =  "Hello jenkins !";
 
     public App() {}
 
